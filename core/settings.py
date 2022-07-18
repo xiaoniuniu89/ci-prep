427f93c29ba90dd76db7e8615310b9018e087809
Shane Muirhead <shane.muirhead@tribusdigital.com>
2022-07-18 13:00:37 +0100

    Updated settings to work locally, for production SECRET_KEY will need setting properly

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ else:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+SECRET_KEY = SECRET_KEY if os.environ.get('SECRET_KEY') else "some-secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
